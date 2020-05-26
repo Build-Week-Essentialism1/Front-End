67fe0ded2af95765f2b4d958210f7ea48758d09e
William Bell <wtb2804@gmail.com>
2020-05-26 13:41:18 -0500

    handlechanges functioning

--- essentialism/src/components/Register.js
+++ essentialism/src/components/Register.js
@@ -1,30 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const initialFormValues = {
+const initialRegisterValues = {
     username: '',
     password: '',
     email:''
 }
 
-const initialFormErrors = {
+const initialRegisterErrors = {
     username: '',
     password: '',
     email:''
 }
 
 function Register() {
+
+    const [user, setUser] = useState(initialRegisterValues)
+
+    // Event Handlers
+    const handleChange = event => {
+        setUser({ ...user, [event.target.name]: event.target.value });
+    };
+    
+    // onSubmit function
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(user.name);
+        console.log(user.password);
+        console.log(user.email)
+    };
+
+
     return (
-        <div className="login">
-            <form>
+        <div className="register">
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="username">Username: </label>
                     <input
                         type="text"
                         name="username"
                         placeholder="username"
+                        value={user.username}
+                        onChange={handleChange}
                         required
-                    // value=""
-                    // onChange=""
                     />
                 </div>
 
@@ -34,9 +52,9 @@ function Register() {
                         type="password"
                         name="password"
                         placeholder="password"
+                        value={user.password}
+                        onChange={handleChange}
                         required
-                    // value=""
-                    // onChange=""
                     />
                 </div>
 
@@ -46,11 +64,12 @@ function Register() {
                         type="email"
                         name="email"
                         placeholder="email"
-                        
-                    // value=""
-                    // onChange=""
+                        value={user.email}
+                        onChange={handleChange}
                     />
                 </div>
+
+                <button>Submit</button>
             </form>
         </div>
     )
