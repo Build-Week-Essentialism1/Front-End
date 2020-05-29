f6b1e35655ee43c1b6b9110965be1782bfdec8c7
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-05-29 13:47:52 -0400

    final push before due time

--- essentialism/src/components/CardComponent.js
+++ essentialism/src/components/CardComponent.js
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../utils/axiousWithAuth";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function CardComponent(props) {
   const [priorities, setPriorities] = useState([]);
+  const history = useHistory();
 
   const getCard = (e) => {
     axiosWithAuth()
@@ -18,19 +20,28 @@ function CardComponent(props) {
       .delete(`https://essentialismapi.herokuapp.com/api/uv/${props.user.id}`)
       .then((res) => {
         console.log(res.data);
-        // history.go(0);
+        history.go(0);
       });
   };
+  // const toggleItem = (value) => {
+  //   if (value.class)
+  // };
+
+  // const AddClass = (value) => {
+  //   const v = document.getElementById(value.id);
+  //   v.classList.add(".done");
+  // };
 
   return (
     <div>
       <button onClick={() => getCard()}> Get Card </button>
+      <button onClick={() => Delete()}> Delete all Values </button>
+
       {priorities.map((item) => (
         <div>
-          <h3>{item.name} </h3>{" "}
+          <h3 id={item.id}>{item.name} </h3>
         </div>
       ))}
-      <button onClick={() => Delete()}> Delete values From Card </button>
     </div>
   );
 }
